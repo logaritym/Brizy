3167142c0b2d0da54b5dc2777d76f5136ac56abf
Alex Zaharia <alecszaharia@gmail.com>
2018-06-04 15:52:37 +0300

    Added pages where the attachmet is used as a meta value to the attachment

--- brizy.php
+++ brizy.php
@@ -11,7 +11,7 @@
  * Domain Path: /languages
  */
 
-define( 'BRIZY_DEVELOPMENT', false );
+define( 'BRIZY_DEVELOPMENT', true );
 define( 'BRIZY_LOG', false );
 define( 'BRIZY_VERSION', '1.0.15' );
 define( 'BRIZY_EDITOR_VERSION', '1.0.39' );
